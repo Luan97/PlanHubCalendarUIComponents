936475e65473ff1e8ed63d463f64c82145c76f9d
Luan97 <luanling97@gmail.com>
2021-12-28 11:56:06 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# PlanHubCalendarUIComponents
+
+A description of this package.
---
+++ Tests/PlanHubCalendarUIComponentsTests/PlanHubCalendarUIComponentsTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import PlanHubCalendarUIComponents
+
+final class PlanHubCalendarUIComponentsTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(PlanHubCalendarUIComponents().text, "Hello, World!")
+    }
+}
